34e4a8a74a65e3c2b8395da817945341c7042c2b
Catalin Amza <amzaionutcatalin@gmail.com>
2021-09-15 17:52:21 +0300

    Work done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, createRef } from 'react';
+import { templateKeys } from './data';
 
 function App() {
+  const refObject = templateKeys.reduce((res, _, letterIndex) => {
+    res[letterIndex] = [];
+    return res;
+  }, {});
+
+  const itemsRef = useRef(refObject);
+
+  templateKeys.forEach((el, letterIndex) => {
+    el.chars.forEach(() => itemsRef.current[letterIndex].push(createRef()))
+  });
+
+  const onClicked = (letterIndex, index) => () => {
+    const currentRef = itemsRef.current[letterIndex][index];
+    console.log(currentRef.current.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {templateKeys.map((el, letterIndex) => (
+        <div key={letterIndex}>
+          <p>{el.letter}</p>
+          {el.chars.map((char, index) => (
+            <input
+              key={index}
+              ref={itemsRef.current[letterIndex][index]}
+              defaultValue={char}
+              onClick={onClicked(letterIndex, index)}
+            />
+          ))}
+        </div>
+      ))}
+    </>
   );
 }
 
